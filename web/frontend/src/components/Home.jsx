c22d1710f6c90175350f4eda6f40742bf70f3900
Harsha1309 <harshyadav1393@gmail.com>
2023-02-10 21:22:06 +0530

    minor changes

--- web/frontend/src/components/Home.jsx
+++ web/frontend/src/components/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Home() {
   return (
     <div>
-        <h1>Welcome to our website</h1>
+        <h1 className='mx-2'>Welcome to our website</h1>
     </div>
   )
 }
