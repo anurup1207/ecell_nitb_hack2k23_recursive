210bccc4f16ef84a3f5469484dfa123cd381031a
Asutosh Pradhan <75766416+Maagztech@users.noreply.github.com>
2023-02-11 12:30:17 +0530

    backend basic completed

--- web/Backend/server/routes/index.ts
+++ web/Backend/server/routes/index.ts
@@ -1,12 +1,10 @@
 import authRouter from "./authRouter";
 import userRouter from "./userRouter";
 import notificationRouter from "./notificationRouter";
-import datarouter from "./dataRouter";
 const routes = [
   authRouter,
   userRouter,
   notificationRouter,
-  datarouter,
 ];
 
 export default routes;
